154553305f9a8aaa71ae9ca13a0ba41fd7c1d948
Senthilkumar Subramanian <xenthilkumar@hotmail.com>
2025-05-04 09:55:55 +0530

    Update createApp.ts

--- src/createApp.ts
+++ src/createApp.ts
@@ -29,6 +29,7 @@ export function createApp() {
     "http://localhost:3000",
     "http://job-card-zaara.s3-website.ap-south-1.amazonaws.com",
     "https://job-card-frontend-ruby.vercel.app",
+    "https://job-card-indol.vercel.app",
     "https://job-card-frontend-oeiowjygx-xenthils-projects.vercel.app"
   ];
   
