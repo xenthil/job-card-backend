1e4116def21104fc9de0adfdf542d3c5409e5f8c
Senthilkumar Subramanian <xenthilkumar@hotmail.com>
2025-05-04 11:29:21 +0530

    Update createApp.ts

--- src/createApp.ts
+++ src/createApp.ts
@@ -21,34 +21,7 @@ declare global {
 export function createApp() {
   const app = express();
   global.__uploadDir = __dirname + "/uploads";
-  global.__srcDir = __dirname;
-
-  const CLIENT_URLS = [
-    "http://localhost:3000",
-    "http://job-card-zaara.s3-website.ap-south-1.amazonaws.com",
-    "https://job-card-frontend-ruby.vercel.app",
-    "https://job-card-indol.vercel.app",
-    "https://job-card-frontend-oeiowjygx-xenthils-projects.vercel.app"
-  ];
-
-  /*app.use(cors({
-    origin: (origin, callback) => {
-      try {
-        if (
-          !origin ||
-          CLIENT_URLS.includes(origin) ||
-          /\.vercel\.app$/.test(new URL(origin).hostname)
-        ) {
-          callback(null, true);
-        } else {
-          callback(new Error('Not allowed by CORS'));
-        }
-      } catch (err) {
-        callback(new Error('CORS origin check failed'));
-      }
-    },
-    credentials: true,
-  }));*/
+  global.__srcDir = __dirname;  
 
   app.use(cors());  
   app.use(cookieParser());
@@ -57,7 +30,6 @@ export function createApp() {
   app.use(compression());
   app.use(express.static(__uploadDir + "/materialInward"));
   app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
 
   app.use("/api/auth", AuthRoute);
   app.use("/api/client", ClientRoute);
